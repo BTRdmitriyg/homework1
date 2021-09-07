c3bdc701f3a70f1e7563aefd1d8c8379827b1590
dmitriyg <dmitriy.gorbatenko@hexagon.com>
2021-09-07 22:44:55 +0700

    just cmake

---
+++ part1/main.cpp
@@ -0,0 +1,11 @@
+#include "versions.h"
+
+#include <iostream>
+
+int main (int, char **)
+{
+    std::cout << "build " << HOMEWORK_VERSION << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
